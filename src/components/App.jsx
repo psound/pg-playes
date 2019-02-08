c889f6de9afec7504cf31295aaea82ba6006d328
Pedro Garcia <pedro@sshsmedia.com>
2019-02-08 12:59:55 -0500

    Fixed login error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import Add from './New';
 const App = () => (
   <Router>
     <div>
-      <Route path="/" exact component={Login} />
+      <Route path="/" exact component={Login} /> 
       <Route path="/register/" component={Register} />
       <Route path="/roster/" component={Roster} />
       <Route path="/player/new/" component={Add} />
